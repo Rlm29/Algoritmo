eb11677328af5b2c20eeaf87bc2e4075e85033db
Rlm29 <145131286+Rlm29@users.noreply.github.com>
2023-12-08 18:21:14 -0300

    Exercicio1.c

--- Avaliação 1/Exercicio1.c
+++ Avaliação 1/Exercicio1.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-float calcular_fx(float x) {
-    float resultado;
-    
-    if (x <= 4) {
-        printf("A raiz quadrada de x^2 - 16 nao %c real.\n", 130);
-        resultado = 0; 
-    } else {
-        resultado = (5 * x + 3) / sqrt(x * x - 16);
-    }
-    
-    return resultado;
-}
 
-main() {
+int main() {
     float x;
-    
+
     printf("Digite o valor de x: ");
     scanf("%f", &x);
 
-    float resultado_fx = calcular_fx(x);
+    float resultado_fx;
 
-    if (resultado_fx != 0) {
-        printf("O valor de f(%.2f) %c igual a %.2f\n", x, 130, resultado_fx);
+    if (x <= 4) {
+        printf("A raiz quadrada de x^2 - 16 nao %c real.\n", 130);
+        resultado_fx = nanf(""); 
+    } else {
+        resultado_fx = (5 * x + 3) / sqrt(x * x - 16);
     }
 
+    if (!isnan(resultado_fx)) {
+        printf("O valor de f(%.2f) %c igual a %.2f\n", x, 130, resultado_fx);
+    }
 }
